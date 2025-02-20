77ae12637f7e32571ff99d4867e228e91172c15e
SyedFaisal30 <sfarz172320@gmail.com>
2025-02-20 19:36:30 +0530

    front end setup

---
+++ frontend/chefgenie/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      {/* <h1>Chef Genie</h1> */}
+      <Header></Header>
+    </>
+  )
+}
+
+export default App
