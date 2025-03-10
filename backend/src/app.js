8af3965642f10872168a38696b044aa9e57144d1
SyedFaisal30 <sfarz172320@gmail.com>
2025-03-10 14:44:08 +0530

    Create post done

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser';
 const app = express();
 
 app.use(cors({
-    origin: 'https://chefgenie.vercel.app',
+    origin: ["https://chefgenie.vercel.app","https://localhost:5173"],
     credentials: true
 }));
 
